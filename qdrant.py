932c499dde79d714bc9fc625c0b8790163ec7223
Hemanth Ganesh villuri <hemanthganeshvilluri@gmail.com>
2026-09-25 20:06:29 +0530

    Refactor vector_database to use QdrantClient

--- qdrant.py
+++ qdrant.py
@@ -1,20 +1,27 @@
+from qdrant_client import QdrantClient
+from qdrant_client.models import Distance, VectorParams
 from langchain_qdrant import QdrantVectorStore
 
 def vector_database(embed_model):
-
-    try:
-        vector_db = QdrantVectorStore.from_existing_collection(
-            embedding = embed_model,
-            path = './qdrant_data',
-            collection_name = 'documents'
+    collection_name = "documents"
+    client = QdrantClient(
+        path = "./qdrant_data"
+    )
+    if not client.collection_exists(collection_name):
+        vector_size = len(
+            embed_model.embed_query("test")
         )
-
-    except Exception:
-        vector_db = QdrantVectorStore.from_documents(
-            documents = [],
-            embedding = embed_model,
-            path = './qdrant_data',
-            collection_name = 'documents'
+        client.create_collection(
+            collection_name = collection_name,
+            vectors_config = VectorParams(
+                size = vector_size,
+                distance = Distance.COSINE
+            )
         )
+    vector_db = QdrantVectorStore(
+        client = client,
+        collection_name = collection_name,
+        embedding = embed_model
+    )
 
     return vector_db
