c7857a100b16ce547ab14d9e0dd3236830215909
Hemanth Ganesh villuri <hemanthganeshvilluri@gmail.com>
2026-09-26 20:05:36 +0530

    Enhance query_pipeline with debug print statements
    Added print statements to display retrieved and reranked chunks along with their counts.

--- query_pipeline.py
+++ query_pipeline.py
@@ -6,8 +6,21 @@ from generation import generation
 
 def query_pipeline(query, vector_db, models):
     top_chunks = retrieval(query, vector_db)
-    rerank_chunks = reranker(query, top_chunks, models['rerank_model'])
+    print("RETRIEVED CHUNKS:", len(top_chunks))
+    for chunk in top_chunks:
+        print(chunk.page_content[:200])
+    rerank_chunks = reranker(
+        query,
+        top_chunks,
+        models['rerank_model']
+    )
+    print("RERANKED CHUNKS:", len(rerank_chunks))
     context = context_format(rerank_chunks)
+    print("CONTEXT:")
+    print(context)
     prompt = prompt_format(query, context)
-    answer = generation(prompt, models['llm'])
+    answer = generation(
+        prompt,
+        models['llm']
+    )
     return query, answer, rerank_chunks
