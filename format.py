660ee4aa5756c015641ef83f5e118d952cd3839e
Hemanth Ganesh villuri <hemanthganeshvilluri@gmail.com>
2026-09-26 23:14:47 +0530

    Refactor document formatting logic for clarity

--- format.py
+++ format.py
@@ -1,13 +1,14 @@
+from docling_core.types.doc import DoclingDocument, TextItem, SectionHeaderItem, ListItem, TableItem, PictureItem
 from image_descripter import generate_text
 def formatting_documents(documents, processor, model):
     results = []
     for file in documents:
         source = file['source']
-        doc = file['document']
+        doc: DoclingDocument = file['document']
         curr_section = None
-        for  element, _ in doc:
-            print("ELEMENT TYPE:", type(element).__name__)
-            if type(element).__name__ == 'SectionHeaderItem':
+        for element, _ in doc.iterate_items():
+            page_no = element.prov[0].page_no if (hasattr(element, 'prov') and element.prov) else None
+            if isinstance(element, SectionHeaderItem):
                 text = element.text
                 curr_section = text
                 results.append({
@@ -15,34 +16,46 @@ def formatting_documents(documents, processor, model):
                     'type': 'heading',
                     'section': curr_section,
                     'source': source,
-                    'page_no': element.prov[0].page_no
+                    'page_no': page_no
                 })
-            elif type(element).__name__ == 'TextItem':
+            elif isinstance(element, TextItem):
                 text = element.text
                 results.append({
                     'content': text,
                     'type': 'text',
                     'section': curr_section,
                     'source': source,
-                    'page_no': element.prov[0].page_no
+                    'page_no': page_no
                 })
-            elif type(element).__name__ == 'TableItem':
-                table = element.export_to_dataframe(doc)
+            elif isinstance(element, ListItem):
+                text = element.text
                 results.append({
-                    'content': table,
+                    'content': text,
+                    'type': 'list_item',
+                    'section': curr_section,
+                    'source': source,
+                    'page_no': page_no
+                })
+            elif isinstance(element, TableItem):
+                table_df = element.export_to_dataframe(doc)
+                results.append({
+                    'content': table_df,
                     'type': 'table',
                     'section': curr_section,
                     'source': source,
-                    'page_no': element.prov[0].page_no
+                    'page_no': page_no
                 })
-            elif type(element).__name__ == "PictureItem":
+            elif isinstance(element, PictureItem):
                 image = element.get_image(doc)
-                image_to_text = generate_text(image, processor, model)
+                if image:
+                    image_to_text = generate_text(image, processor, model)
+                else:
+                    image_to_text = ""
                 results.append({
                     'content': image_to_text,
-                    "type": 'image',
-                    "section": curr_section,
-                    "source": source,
-                    "page_no": element.prov[0].page_no
-            })
+                    'type': 'image',
+                    'section': curr_section,
+                    'source': source,
+                    'page_no': page_no
+                }) 
     return results
