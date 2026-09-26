77596600a2d60f42ad37142333ffec4a8b2688ad
Hemanth Ganesh villuri <hemanthganeshvilluri@gmail.com>
2026-09-26 20:30:23 +0530

    Refactor loop to unpack element in format.py

--- format.py
+++ format.py
@@ -5,7 +5,7 @@ def formatting_documents(documents, processor, model):
         source = file['source']
         doc = file['document']
         curr_section = None
-        for element in doc:
+        for _, element in doc:
             print("ELEMENT TYPE:", type(element).__name__)
             if type(element).__name__ == 'SectionHeaderItem':
                 text = element.text
